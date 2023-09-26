eccee8320065008f514d3531dd6d75a2499b86a8
Monster <mauriciogi@utp.edu.co>
2023-09-26 14:47:41 -0500

    ahora se guarda el usuario en ./user.json, por ahora solo se almacena el creado

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 //Agregar estos nombres hace que sea mas flexible que una tupla ya que no depende del orden para acceder a los valores 
 mod user;
 use crate::user::structure::{User, UserRole};
-use crate::user::newuser::{new_user, add_user_data};
+use crate::user::newuser::{new_user, add_user_data, write_json_user};
 use crate::user::admin::{new_admin, admin_acces};
 use crate::user::output::show_user;
 
@@ -12,7 +12,8 @@ fn main(){
     user = add_user_data();
 
     let user: User = new_user(user.0, user.1, user.2,user.3);
-    let admin: bool = admin_acces(&user.role); 
+    let admin: bool = admin_acces(&user.role);
+    let _result = write_json_user(&user, "./users.json").expect("Error");
     show_user(&user.name, &user.email, &user.age, &user.active, admin,&user.description);
     
     let useradmin: User = new_admin(user.name, user.email, user.age, user.role, user.description);
--- src/user/newuser.rs
+++ src/user/newuser.rs
@@ -1,6 +1,14 @@
 
+extern crate serde;
+extern crate serde_derive;
+extern crate serde_json;
+
 use std::io;
 use std::str::FromStr;
+use std::fs::File;
+use std::io::Write;
+
+
 
 use crate::User;
 use crate::UserRole;
@@ -18,34 +26,55 @@ pub fn new_user(name: String, email:String, age: u16,description:Option<String>)
 }
 
 pub fn add_user_data()-> (String, String, u16,Option<String>) {
+
     let mut tupla: (String, String, u16,Option<String>)=("".to_string(),"".to_string(),0,Some("".to_string()));
     println!("Ingrese el nombre: ");
     let mut input = String::new();
     io::stdin().read_line(&mut input).ok().expect("Error al leer de teclado");
-    tupla.0 = input;
+    tupla.0 = input.trim().to_string();
+
     println!("Ingrese el email: ");
     let mut input = String::new();
     io::stdin().read_line(&mut input).ok().expect("Error al leer de teclado");
-    tupla.1 = input;
+    tupla.1 = input.trim().to_string();
+
     println!("Ingrese la edad: ");
     let mut input = String::new();
     io::stdin().read_line(&mut input).ok().expect("Error al leer de teclado");
     let edad: u16 = u16::from_str(&input.trim()).unwrap();
     tupla.2 = edad;
+
     println!("Desea ingresar una descripción?");
     println!("ingresar información: 1 \n No ingresar información: 0");
     let mut input = String::new();
     io::stdin().read_line(&mut input).ok().expect("Error al leer de teclado");
     let i: u16 = u16::from_str(&input.trim()).unwrap();
+
     if i == 1 {
+
         println!("Ingrese una descripción personal: ");
         let mut input = String::new();
         io::stdin().read_line(&mut input).ok().expect("Error al leer de teclado");
-        let description = Some(input.to_string());
+        let description = Some(input.trim().to_string());
         tupla.3 = description;
+
     }else if i == 0 {
+
         let description: Option<String> = None;
         tupla.3 = description;
+
     }
+
     return tupla;
 }
+
+pub fn write_json_user(user:&User, user_file: &str) -> Result<(), Box<dyn std::error::Error>> {
+
+    let user_json = serde_json::to_string(user)?;
+    let mut file = File::create(user_file)?;
+    file.write_all(user_json.as_bytes())?;
+
+    Ok(())
+
+}
+
